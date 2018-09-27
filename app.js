51e00d3c000439a2e4ef56eb9b6b03c2e6699622
Michael Qin <mike.qin@gmail.com>
2018-09-27 00:09:15 -0400

    Update app.js

--- app.js
+++ app.js
@@ -33,18 +33,33 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.post("/signup", function(req, res) {
-    var claims = req.body;
+    if (!claims.email) {
+        res.status(400).json({ error: 'email is not defined!'});
+    }
+
+    if (!claims.password) {
+        res.status(400).json({ error: 'password is not defined!'});
+    }
+       
+    if (!claims.firstName) {
+        res.status(400).json({ error: 'firstName is not defined!'});
+    }
+
+    if (!claims.lastName) {
+        res.status(400).json({ error: 'lastName is not defined!'});
+    }    
+
+    if (claims.password == '1234') {
+        res.status(400).json({ error: 'password is too simple!' });
+    }
+    else {
+        res.status(200).json({ status: '1' });
+    }
+
     console.log("email: " + claims.email);
     console.log("password: " + claims.password);
     console.log("firstName: " + claims.firstName);
     console.log("lastName: " + claims.lastName);
-
-    if (claims.password == '1234') {
-    	res.status(400).json({ error: 'Password is too simple!' });
-    }
-    else {
-    	res.status(200).json({ status: '1' });
-    }
 });
 
 app.listen(port, () => console.log("/signup API: Listening on port " + port));
