abfa17a8979341b323c5738500323c43945929d5
Michael Qin <mike.qin@gmail.com>
2018-09-27 01:24:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-// Authors: Michael Qin
-// https://github.com/MikeQin/rest-api
+/**
+  Authors: Michael Qin
+  https://github.com/MikeQin/rest-api
+*/
 const express = require("express");
 const app = express();
 const basicAuth = require('express-basic-auth')
@@ -8,9 +10,9 @@ const bodyParser = require('body-parser');
 
 app.use(basicAuth({
     users: {
-    	'admin': 'admin',
-    	'user': 'user',
-    	'B2cRestClientId': 'admin'
+        'admin': 'admin',
+        'user': 'user',
+        'B2cRestClientId': 'admin'
     },
     unauthorizedResponse: getUnauthorizedResponse,
     realm: 'Azure B2C REST API Demo'
@@ -19,47 +21,63 @@ app.use(basicAuth({
 function getUnauthorizedResponse(req) {
     var msg;
     if (req.auth) {
-	msg = 'Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected!';
+    msg = 'Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected!';
     }
     else {
-	msg = 'No credentials provided!';
+    msg = 'No credentials provided!';
     }
     console.log('[*] ' + msg);
     
     return msg;
 }
 
+function logClaims(claims) {
+    console.log("---- log claims ---- ");
+    console.log("email: " + claims.email);
+    console.log("password: " + claims.password);
+    console.log("firstName: " + claims.firstName);
+    console.log("lastName: " + claims.lastName);
+}
+
+function validateClaims(claims, errors) {
+    if (!claims.email) {
+        errors.push('email is not defined!');
+    }
+
+    if (!claims.password) {
+        errors.push('password is not defined!');
+    }
+       
+    if (!claims.firstName) {
+        errors.push('firstName is not defined!');
+    }
+
+    if (!claims.lastName) {
+        errors.push('lastName is not defined!');
+    }    
+
+    if (claims.password == '1234') {
+        errors.push('password is too simple!');
+    }    
+}
+
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.post("/signup", function(req, res) {
-    if (!claims.email) {
-        res.status(400).json({ error: 'email is not defined!'});
-    }
+    var claims = req.body;
+    var errorsArr = new Array();
 
-    if (!claims.password) {
-        res.status(400).json({ error: 'password is not defined!'});
-    }
-       
-    if (!claims.firstName) {
-        res.status(400).json({ error: 'firstName is not defined!'});
-    }
+    logClaims(claims);
+    validateClaims(claims, errorsArr);
 
-    if (!claims.lastName) {
-        res.status(400).json({ error: 'lastName is not defined!'});
-    }    
-
-    if (claims.password == '1234') {
-        res.status(400).json({ error: 'password is too simple!' });
+    if (errorsArr.length > 0) {
+        res.status(400).json({ errors: errorsArr });
     }
     else {
         res.status(200).json({ status: '1' });
     }
 
-    console.log("email: " + claims.email);
-    console.log("password: " + claims.password);
-    console.log("firstName: " + claims.firstName);
-    console.log("lastName: " + claims.lastName);
 });
 
 app.listen(port, () => console.log("/signup API: Listening on port " + port));
