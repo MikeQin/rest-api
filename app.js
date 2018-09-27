90e4ca73f332666430aa679907f68d8ccded6c77
Michael Qin <mike.qin@gmail.com>
2018-09-26 23:45:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,18 +12,20 @@ app.use(basicAuth({
     	'B2cRestClientId': 'admin'
     },
     unauthorizedResponse: getUnauthorizedResponse,
-    realm: 'Azure B2C REST API Demo',
+    realm: 'Azure B2C REST API Demo'
 }));
+
 function getUnauthorizedResponse(req) {
-	var msg;
-	if (req.auth) {
-		msg = 'Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected!';
-	}
-	else {
-		msg = 'No credentials provided!';
-	}
-	console.log('[*] ' + msg);
-	return msg;
+    var msg;
+    if (req.auth) {
+	msg = 'Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected!';
+    }
+    else {
+	msg = 'No credentials provided!';
+    }
+    console.log('[*] ' + msg);
+    
+    return msg;
     /*return req.auth
         ? ('Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected')
         : 'No credentials provided';*/
@@ -33,10 +35,10 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.post("/signup", function(req, res) {
-	var claims = req.body;
-	console.log("email: " + claims.email);
-	console.log("password: " + claims.password);
-	console.log("firstName: " + claims.firstName);
+    var claims = req.body;
+    console.log("email: " + claims.email);
+    console.log("password: " + claims.password);
+    console.log("firstName: " + claims.firstName);
     console.log("lastName: " + claims.lastName);
 
     if (claims.password == '1234') {
@@ -44,7 +46,7 @@ app.post("/signup", function(req, res) {
     }
     else {
     	res.status(200).json({ status: '1' });
-	}
+    }
 });
 
 app.listen(port, () => console.log("/signup API: Listening on port " + port));
